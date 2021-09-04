53e4814453467ffec4a2db311ec7d6d76f6f61f6
Luis Vitor <luis.vitor@dcx.ufpb.br>
2021-09-04 00:23:41 -0300

    scraper para pegar trabalhos no indeeed

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Indeed-Job-Scraper
 Web Scraper for 2º project 'Indeed Job' (https://github.com/VitorAmrm/Web-Scraping-Projects);
+
--- script/indeed_scraper.pl
+++ script/indeed_scraper.pl
@@ -1,7 +1,10 @@
 use strict;
-use WWW::Selenium;
 use Web::Scraper;
+use WWW::Mechanize;
+use HTML::TreeBuilder;
 use DBI;
+use Encode;
+use URI;
 
 #Database
 my $driver = 'SQLite';
@@ -9,12 +12,12 @@ my $path_db = './database/indeed_scraper.sqlite';
 
 my $dbh = DBI -> connect("DBI:$driver:dbname=$path_db","","",{ RaiseError => 1 }) or die $DBI::errstr;
 
+
 my $table_create = qq( CREATE TABLE indeed_scraper ( 
     job_title TEXT,
     company TEXT,
     location TEXT,
     post_date TEXT,
-    extract_date TEXT,
     summary TEXT,
     salary TEXT
     ) );
@@ -22,19 +25,73 @@ my $table_create = qq( CREATE TABLE indeed_scraper (
 my $exec_create = $dbh -> do($table_create) or die $DBI::errstr;
 #######
 
-#Selenium
+#MECHANIZE
+my $url = 'https://br.indeed.com/';
+my $mech = WWW::Mechanize->new();
+my $search = 'mechanize';
 
-my $path_to_browser = "";
+my $job = "vendas";
 
-my $webdriver = WWW::Selenium -> new (
-    host => "localhost",
-    port => "8080",
-    browser => $path_to_browser,
-    browser_url => "http://www.google.com" 
-);
+$mech->get( $url );
+$mech->form_number(1);
+$mech->field('q',$job);
+$mech->click();
 
-
+#SCRAPER&TreeBuilder
 
 
-$webdriver -> stop;
+my $start_list = scraper {
+    process 'div[class="pagination"] ul li', "btns[]" => scraper {
+
+      process "a", uri => '@href';
+    
+    };
+};
+ 
+my $res = $start_list->scrape( Encode::encode("utf8","$mech->{content}") );
+
+
+my $save_db = scraper {
+
+    process 'div[class="job_seen_beacon"]',"jobs[]" => scraper {
+
+        process 'h2', "job_title" => 'TEXT',
+        process 'span[class="companyName"]',"company" => 'TEXT',
+        process 'div[class="companyLocation"]', "location" => 'TEXT',
+        process 'span[class="date"]',"post_date" => 'TEXT', 
+        process 'div[class="job-snippet"]',"summary" => 'TEXT',
+        process 'span[class="salary-snippet"]', "salary" => 'TEXT' 
+    };
+};
+
+my @uris_to_go;
+#retirar URI iguais
+for my $author (@{$res->{btns}}) {
+    push @uris_to_go, $author if !grep{"$_->{uri}" eq "$author->{uri}"}@uris_to_go;
+}
+
+my $cont = 0;
+
+for my $uris (@uris_to_go){
+
+    print $cont;
+    $cont += 1;
+
+    my $to_go = substr($url,0,-1)."$uris->{uri}"."\n";
+
+    my $content = $save_db -> scrape(URI -> new($to_go));
+
+    for my $jobs (@{$content->{jobs}}){
+
+        my $insert = qq(
+            INSERT INTO indeed_scraper ( job_title, company, location, post_date, summary, salary)
+            VALUES("$jobs->{job_title}","$jobs->{company}","$jobs->{location}","$jobs->{post_date}","$jobs->{summary}", "$jobs->{salary}")
+        );
+
+        my $exec_insert = $dbh -> do ($insert)or die $DBI::errstr;
+    };
+
+}
+
+
 $dbh -> disconnect();
